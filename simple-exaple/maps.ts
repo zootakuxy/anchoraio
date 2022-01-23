87ff60576f1665e3a07ecc32f8ad4b46b20e281d
Daniel Costa <costa.xdaniel@gmail.com>
2022-01-23 20:40:09 +0000

    simle example

--- simple-exaple/maps.ts
+++ simple-exaple/maps.ts
@@ -1,17 +1,26 @@
 export const serverHost = process.argv[2];
 export const identifier = process.argv[3];
+export const server = process.argv[4];
 export const agentPort = 8080;
 export const serverPort = 48000;
 export const serverAnchorPort = 48001
 
 export const hosts = {
     //web
-    "127.100.1.1": { server: identifier, application: 80 },
-    "127.100.2.2": { server: identifier, application: "web" },
+    "127.100.1.1": { server: server, application: 80 },
+    "127.100.2.2": { server: server, application: "web" },
 
     //postgres
-    "127.100.1.3": { server: identifier, application: 5432 },
-    "127.100.1.4": { server: identifier, application: "postgres" },
+    "127.100.1.3": { server: server, application: 5432 },
+    "127.100.1.4": { server: server, application: "postgres" },
+
+    //web
+    "127.200.1.1": { server: identifier, application: 80 },
+    "127.200.2.2": { server: identifier, application: "web" },
+
+    //postgres
+    "127.200.1.3": { server: identifier, application: 5432 },
+    "127.200.1.4": { server: identifier, application: "postgres" },
 
 }
 
