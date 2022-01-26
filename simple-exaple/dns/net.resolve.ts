74fe2c110abba59e294f65bfc3a141279fda04f6
Daniel Costa <costa.xdaniel@gmail.com>
2022-01-26 22:03:57 +0000

    simle example

--- simple-exaple/dns/net.resolve.ts
+++ simple-exaple/dns/net.resolve.ts
@@ -2,13 +2,18 @@ import {DnsAnswer} from "dns2";
 const { TCPClient } = require('dns2');
 
 let clients = [
-    "1.1.1.1",
     "8.8.8.8",
-    "8.8.4.4"
+    "8.8.4.4",
+    "1.1.1.1"
 ];
 
 export const netResolve = new class NetResolver {
-    dnsResolves = clients.map( value => TCPClient( value ) );
+    dnsResolves = clients.map( name => {
+        return {
+            resolve: TCPClient( name ),
+            name
+        }
+    } );
     resolves:{ [domain:string]:DnsAnswer[] } = {};
     async resolve( domainName:string ):Promise<DnsAnswer[]>{
         return new Promise( (_resolve, reject) => {
@@ -21,10 +26,10 @@ export const netResolve = new class NetResolver {
             let exists = this.resolves[ domainName ];
             if( exists ) return Promise.resolve( exists );
 
-            this.dnsResolves.forEach( resolver => {
-                resolver( domainName ).then( ( result )=>{
+            this.dnsResolves.forEach( dns => {
+                dns.resolve( domainName ).then( ( result )=>{
                     if( result.answers.length > 0 ) resolve( result.answers );
-                })
+                }).catch( reason => console.error( dns.name, reason ))
             });
         })
     }
