d3f62dfb33082ca1b2d7a714cbda76c706449470
Daniel Costa <costa.xdaniel@gmail.com>
2022-01-26 18:50:06 +0000

    simle example

--- simple-exaple/maps.ts
+++ simple-exaple/maps.ts
@@ -4,6 +4,6 @@ export const serverHost = process.argv[2];
 export const identifier = asAio( process.argv[3] ).identifier;
 export const agentPort = 8888;
 export const serverPort = 48000;
-export const serverAnchorPort = 48001
+export const serverAnchorPort = 48001;
 
 console.log( "identifier", identifier);
