92bb7aa67dc36203b26c1972f5670982a2f60f73
Daniel Costa <costa.xdaniel@gmail.com>
2022-05-27 12:47:38 +0000

    simle example

--- core/global/init.ts
+++ core/global/init.ts
@@ -1,13 +1,1 @@
-(()=>{
-    require( "source-map-support" ).install();
-    process.on( "uncaughtExceptionMonitor", error => {
-        // console.error(error.message)
-    });
-    process.on( "uncaughtException", error => {
-        // console.error(error.message)
-    });
-    process.on( "unhandledRejection", error => {
-        // console.error(error)
-    });
-    console.log("[ANCHORIO] Init> OK!");
-})();
+require( "source-map-support" ).install();
