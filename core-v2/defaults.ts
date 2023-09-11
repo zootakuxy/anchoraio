687ae8c7e300c5361ca5dd8038b447529de4c460
Daniel Costa <danielcarvalho555@gmail.com>
2023-09-11 19:03:15 +0000

    TSC -w

--- core-v2/defaults.ts
+++ core-v2/defaults.ts
@@ -1,17 +1,55 @@
 import path from "path";
+import {AppProtocol} from "./protocol";
 
-export const Defaults = {
+
+const protocols:{
+    [k in AppProtocol ]: {
+        serverRelease: number,
+        getawayRelease: number
+    }
+} = {
+    http: {
+        serverRelease: 5 as const,
+        getawayRelease: 10 as const
+    } as const, https: {
+        serverRelease: 5 as const,
+        getawayRelease: 10 as const
+    } as const,
+    aio: {
+        serverRelease: 2 as const,
+        getawayRelease: 2 as const
+    } as const,
+    pg: {
+        serverRelease: 1 as const,
+        getawayRelease: 1 as const
+    } as const
+} as const;
+
+const _def =  {
     //language=file-reference
     envFile: path.join(__dirname, "../etc/anchorio.conf" ),
     etc: path.join(__dirname, "../../etc/entry" ),
-    anchorPort:  36900,
-    authPort:  36910,
-    requestPort:  36920,
-    responsePort:  36930,
-    restoreTimeout: 3500,
-    serverHost: "127.0.0.1",
-    releases: 2,
-    getawayRelease: 3,
+    anchorPort:  36900 as const,
+    authPort:  36910 as const,
+    requestPort:  36920 as const,
+    responsePort:  36930 as const,
+    restoreTimeout: 3500 as const,
+    serverHost: "127.0.0.1" as const,
+    serverRelease: 2 as const,
+    getawayRelease: 3 as const,
     getawayReleaseTimeout: 1000 * 60 * 1.5,
     requestTimeout: 1000 * 30,
-}
+    protocol: protocols
+};
+
+
+export const Defaults: {
+    readonly [p in keyof typeof _def ]: typeof _def[p]
+} = new Proxy(_def, {
+    set(target, p: string | symbol, newValue: any, receiver: any): boolean {
+        return false;
+    }
+});
+
+
+
