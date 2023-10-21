0fa40b22333aaff4ce0adbb9bd8abc2029ae804b
Daniel Costa <danielcarvalho555@gmail.com>
2023-10-21 10:32:45 +0000

    TSC -w

--- core-v2/server/services/auth.service.ts
+++ core-v2/server/services/auth.service.ts
@@ -91,8 +91,8 @@ export class AuthService extends BaseEventEmitter<AuthServiceEvent>{
                     delete this.saio.waitConnections[ auth.agent ];
                     delete this.saio.serverSlots[ auth.agent ];
 
-                    let CHECK_TIMEOUT_LIVE = 1000 * 6;
-                    let CHECK_TIMEOUT_LIVE_WAIT = 1000 * 3;
+                    let CHECK_TIMEOUT_LIVE = 1000 * 8;
+                    let CHECK_TIMEOUT_LIVE_WAIT = 1000 * 5;
 
                     let checkAliveTimeOut = () =>{
                         console.log( `Check connection alive with ${ auth.agent }...` )
