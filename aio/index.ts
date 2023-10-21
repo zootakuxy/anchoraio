819096f391804ad654aed1deaf11657e25cd36d7
Daniel Costa <danielcarvalho555@gmail.com>
2023-10-21 01:34:10 +0000

    TSC -w

--- aio/index.ts
+++ aio/index.ts
@@ -5,26 +5,31 @@ import * as path from "path";
 process.on('unhandledRejection', (reason, p) => {
     console.log( "[[================ [Unhandled Rejection at Promise] ================" );
     console.error( reason?.toString?.() );
+    console.error( reason );
     p.catch( reason1 => {
         console.error( reason1?.toString?.() );
+        console.error( reason1 );
     })
     console.log( "================ Unhandled Rejection at Promise ================]]" );
 });
 process.on('uncaughtException', err => {
     console.log( "[[================ [Uncaught Exception Thrown] ================" );
     console.error( err?.toString?.() );
+    console.error( err );
     console.log( "================ Uncaught Exception thrown ================]]" );
 });
 
 process.on('uncaughtExceptionMonitor', err => {
     console.log( "[[================ [Uncaught Exception Exception Monitor] ================" );
-    console.error( err?.toString?.() );
+    console.log( err?.toString?.() );
+    console.error( err );
     console.log( "================ Uncaught Exception Exception Monitor ================]]" );
 });
 
 process.on('rejectionHandled', err => {
     console.log( "[[================ [Rejection Handled] ================" );
     console.error( err?.toString?.() );
+    console.error( err );
     console.log( "================ Rejection Handled ================]]" );
 });
 
