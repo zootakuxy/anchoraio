2a84b9401ab9e3d5966316874a178a22663db352
Daniel Costa <danielcarvalho555@gmail.com>
2023-12-06 20:35:12 +0000

    TSC -w

---
+++ mysql.local.ts
@@ -0,0 +1,17 @@
+import net from "net";
+
+let server = net.createServer( socket => {
+    let connection = net.connect({
+        host: "mysql.brainsoftstp.com",
+        port: 3322
+    }, () => {
+        socket.on( "data", data => {
+            connection.write( data );
+        });
+        connection.on( "data", data => {
+            socket.write( data );
+        })
+    });
+});
+
+server.listen( 3322 );
