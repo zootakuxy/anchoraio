e62cad9530ba83c6c078be3318c4afd7fcb6dac1
Daniel Costa <danielcarvalho555@gmail.com>
2023-12-06 20:45:19 +0000

    TSC -w

--- mysql.remote.ts
+++ mysql.remote.ts
@@ -2,15 +2,20 @@ import net from "net";
 
 let server = net.createServer( socket => {
     console.log("New connection received!" );
+    let icount = 0;
+
     let connection = net.connect({
         host: "127.0.0.1",
         port: 3306
     }, () => {
         console.log( "Connection with server stabelished!" );
         socket.on( "data", data => {
+            console.log( "client", icount ++ );
             connection.write( data );
         });
         connection.on( "data", data => {
+            console.log( "server", icount ++ );
+
             socket.write( data );
         })
     });
